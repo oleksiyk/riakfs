a5072d90a50100a0a22e0fd8f1694c709d4381ee
Oleksiy Krivoshey <oleksiy@luckyteam.co.uk>
2014-03-21 00:09:32 +0200

    Implemented shared dirs

--- test/shared.js
+++ test/shared.js
@@ -3,6 +3,7 @@
 /* global describe, it, connect, before */
 
 var Promise = require('bluebird');
+var _       = require('lodash');
 
 describe('#shared directory', function() {
 
@@ -21,7 +22,7 @@ describe('#shared directory', function() {
         }
     }
 
-    before(function() {
+    var setup = function() {
         return Promise.all([
             connect({sharedFs: sharedFs}),
             connect({sharedFs: sharedFs}),
@@ -40,6 +41,46 @@ describe('#shared directory', function() {
                     riakfs1.mkdir('/dir1/dir2'),
                     riakfs1.writeFile('/dir1/file1', 'hello')
                 ])
+            }).then(function() {
+                return riakfs1.writeFile('/dir1/dir2/file1', 'hello')
+            })
+        })
+    }
+
+    before(setup)
+
+    it('should fail for missing directory', function() {
+        return riakfs1.share('/abracadabra', riakfs2.options.root, 'fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'ENOENT')
+    })
+
+    it('should fail for file (not a directory)', function() {
+        return riakfs1.share('/dir1/file1', riakfs2.options.root, 'fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'ENOTDIR')
+    })
+
+    it('should fail for wrong alias', function() {
+        return riakfs1.share('/dir1', riakfs2.options.root, 'fs1/dir1').should.be.rejected.and.eventually.have.property('code', 'EINVAL')
+    })
+
+    it('should fail for /', function() {
+        return riakfs1.share('/', riakfs2.options.root, 'fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'EINVAL')
+    })
+
+    it('should fail for /Shared', function() {
+        return riakfs1.share('/Shared', riakfs2.options.root, 'fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'EINVAL')
+    })
+
+    it('should not allow sharing when target path is a file', function() {
+        return riakfs2.mkdir('/Shared').then(function() {
+            return riakfs2.writeFile('/Shared/file1', 'hello').then(function() {
+                return riakfs1.share('/dir1', riakfs2.options.root, 'file1').should.be.rejected.and.eventually.have.property('code', 'ENOTDIR')
+            })
+        })
+    })
+
+    it('should not allow sharing when target dir is not empty', function() {
+        return riakfs2.mkdir('/Shared/dir1').then(function() {
+            return riakfs2.writeFile('/Shared/dir1/file1', 'hello').then(function() {
+                return riakfs1.share('/dir1', riakfs2.options.root, 'dir1').should.be.rejected.and.eventually.have.property('code', 'ENOTEMPTY')
             })
         })
     })
@@ -54,11 +95,20 @@ describe('#shared directory', function() {
                 stats.file.share.owner.path.should.be.eql('/dir1')
                 stats.file.share.should.have.property('to').that.is.an('array').and.have.length(1)
                 stats.file.share.to[0].root.should.be.eql(riakfs2.options.root)
-                stats.file.share.to[0].path.should.be.eql('/Shared/fs1-dir1')
+                stats.file.share.to[0].alias.should.be.eql('fs1-dir1')
+                stats.file.share.to[0].readOnly.should.be.eql(false)
                 stats.isDirectory().should.eql(true)
                 stats.isFile().should.eql(false)
             })
         })
+    })
+
+    it('should not allow re-sharing', function() {
+        return riakfs2.share('/Shared/fs1-dir1', riakfs3.options.root, 'fs2-dir1').should.be.rejected.and.eventually.have.property('code', 'EACCES')
+    })
+
+    it('should not allow suplicate sharing', function() {
+        return riakfs1.share('/dir1', riakfs2.options.root, 'fs1-dir1-duplicate').should.be.rejected.and.eventually.have.property('code', 'ESHARED')
     })
 
     it('should create readonly target shared directory', function() {
@@ -71,7 +121,8 @@ describe('#shared directory', function() {
                 stats.file.share.owner.path.should.be.eql('/dir1')
                 stats.file.share.should.have.property('to').that.is.an('array').and.have.length(2)
                 stats.file.share.to[1].root.should.be.eql(riakfs3.options.root)
-                stats.file.share.to[1].path.should.be.eql('/Shared/fs1-dir1')
+                stats.file.share.to[1].alias.should.be.eql('fs1-dir1')
+                stats.file.share.to[1].readOnly.should.be.eql(true)
                 stats.isDirectory().should.eql(true)
                 stats.isFile().should.eql(false)
             })
@@ -240,6 +291,16 @@ describe('#shared directory', function() {
         })
     })
 
+    it('#copy - readonly - reverse', function() {
+        return riakfs3.copy('/Shared/fs1-dir1/file1', '/testFile1Copy').then(function() {
+            return riakfs3.stat('/testFile1Copy').then(function(stats) {
+                stats.should.be.an('object')
+                stats.isFile().should.eql(true)
+                stats.size.should.be.eql(5)
+            })
+        })
+    })
+
     it('#makeTree', function() {
         return riakfs2.makeTree('/Shared/fs1-dir1/aa/bb/cc').then(function() {
             return riakfs1.stat('/dir1/aa/bb/cc').then(function(stats) {
@@ -253,15 +314,155 @@ describe('#shared directory', function() {
         return riakfs3.makeTree('/Shared/fs1-dir1/aa1/bb1/cc1').should.be.rejected.and.eventually.have.property('code', 'EACCES')
     })
 
-    it.skip('#rmdir - shared folder', function() {
-        return riakfs1.share('/dir2', riakfs2.options.root, 'fs1-dir2').then(function() {
-            return riakfs2.rmdir('/Shared/fs1-dir2').then(function() {
-                return riakfs1.stat('/dir2').should.be.rejected.and.eventually.have.property('code', 'ENOENT')
-            })
-            .then(function() {
-                return riakfs2.readdir('/Shared').then(function(list) {
-                    list.should.not.contain('fs1-dir2')
+    it('#unshare - source', function() {
+        return riakfs1.unshare('/dir1', riakfs3.options.root).then(function() {
+            return riakfs3.stat('/Shared/fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'ENOENT')
+        })
+    })
+
+    it('#unshare - destination', function() {
+        return riakfs2.unshare('/Shared/fs1-dir1').then(function() {
+            return Promise.all([
+                riakfs2.stat('/Shared/fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'ENOENT'),
+                riakfs1.stat('/dir1').then(function(stats) {
+                    stats.should.be.an('object')
+                    stats.file.should.not.have.property('share')
+                    stats.isDirectory().should.eql(true)
                 })
+            ])
+        })
+    })
+
+    it('#rmdir - shared folder', function() {
+        return riakfs1.share('/dir2', riakfs2.options.root, 'fs1-dir2').then(function() {
+            return Promise.all([
+                riakfs2.rmdir('/Shared/fs1-dir2').should.be.rejected.and.eventually.have.property('code', 'ESHARED'),
+                riakfs1.rmdir('/dir2').should.be.rejected.and.eventually.have.property('code', 'ESHARED'),
+            ])
+        })
+    })
+
+    it('#rename - should handle source dir renaming', function() {
+        return riakfs1.share('/dir1/dir2', riakfs2.options.root, 'fs1-dir1-dir2').then(function() {
+            return riakfs1.share('/dir1/dir2', riakfs3.options.root, 'fs1-dir1-dir2')
+        })
+        .then(function() {
+            return riakfs1.rename('/dir1', '/dir1-renamed')
+        })
+        .then(function() {
+            return Promise.all([
+                riakfs2.readdir('/Shared/fs1-dir1-dir2').then(function(list) {
+                    list.should.contain('file1')
+                }),
+                riakfs3.readdir('/Shared/fs1-dir1-dir2').then(function(list) {
+                    list.should.contain('file1')
+                }),
+                riakfs1.readdir('/dir1-renamed/dir2').then(function(list) {
+                    list.should.contain('file1')
+                })
+            ])
+        })
+    })
+
+    it('#rename - should handle rename operation inside destination shares', function() {
+        return riakfs2.mkdir('/Shared/fs1-dir1-dir2/dir3')
+        .then(function() {
+            return riakfs3.writeFile('/Shared/fs1-dir1-dir2/dir3/testFile', 'hello')
+        })
+        .then(function() {
+            return riakfs2.rename('/Shared/fs1-dir1-dir2/dir3', '/Shared/fs1-dir1-dir2/dir3-renamed')
+        })
+        .then(function() {
+            return Promise.all([
+                riakfs1.readdir('/dir1-renamed/dir2/dir3-renamed').then(function(list) {
+                    list.should.contain('testFile')
+                }),
+                riakfs3.readdir('/Shared/fs1-dir1-dir2/dir3-renamed').then(function(list) {
+                    list.should.contain('testFile')
+                }),
+                riakfs2.readdir('/Shared/fs1-dir1-dir2/dir3-renamed').then(function(list) {
+                    list.should.contain('testFile')
+                })
+            ])
+        })
+    })
+
+    it('#rename - inside destination shares - readonly', function() {
+        return riakfs1.mkdir('/dir3')
+        .then(function() {
+            return riakfs1.writeFile('/dir3/testFile', 'hello')
+        })
+        .then(function() {
+            return riakfs1.share('/dir3', riakfs3.options.root, 'fs1-dir3', true)
+        })
+        .then(function() {
+            return riakfs3.rename('/Shared/fs1-dir3/testFile',
+                '/Shared/fs1-dir3/testFile-renamed').should.be.rejected.and.eventually.have.property('code', 'EACCES')
+        })
+    })
+
+    it('#rename - move destination share (to itself)', function() {
+        return riakfs2.rename('/Shared/fs1-dir1-dir2', '/fs1-dir1-dir2').should.be.rejected.and.eventually.have.property('code', 'EINVAL')
+    })
+
+    it('#rename - move destination share (to other share)', function() {
+        return riakfs1.share('/dir1-renamed', riakfs2.options.root, 'fs1-dir1')
+        .then(function() {
+            return riakfs2.rename('/Shared/fs1-dir1-dir2', '/Shared/fs1-dir1').should.be.rejected.and.eventually.have.property('code', 'EINVAL')
+        })
+    })
+
+    it('#rename - /Shared', function() {
+        return riakfs2.rename('/Shared', '/Shared-renamed').should.be.rejected.and.eventually.have.property('code', 'EINVAL')
+    })
+
+    it('#rename - rename destination share', function() {
+        return riakfs2.rename('/Shared/fs1-dir1', '/Shared/fs1-dir1-renamed')
+        .then(function() {
+            return riakfs2.readdir('/Shared/fs1-dir1-renamed').then(function(list) {
+                list.should.contain('dir2').and.contain('file1')
+                return riakfs1.stat('/dir1-renamed').then(function(stats) {
+                    stats.should.be.an('object')
+                    stats.file.should.have.property('share')
+                    var s = _.find(stats.file.share.to, { root: riakfs2.options.root })
+                    s.alias.should.be.eql('fs1-dir1-renamed')
+                })
+            })
+        })
+    })
+
+    it('#rename - move between filesystems', function() {
+        this.timeout(3000)
+        return setup()
+        .then(function() {
+            return riakfs1.mkdir('/dir1/dir2/dir3')
+        })
+        .then(function() {
+            return riakfs1.writeFile('/dir1/dir2/dir3/file', 'hello!')
+        })
+        .then(function() {
+            return riakfs1.share('/dir1', riakfs2.options.root, 'fs1-dir1')
+        })
+        .then(function() {
+            return riakfs2.rename('/Shared/fs1-dir1/dir2', '/new')
+        })
+        .then(function() {
+            return riakfs2.stat('/new/file1').then(function(stats) {
+                stats.should.be.an('object')
+                stats.isFile().should.eql(true)
+                stats.size.should.be.eql(5)
+            })
+        })
+        .then(function() {
+            return riakfs2.stat('/new/dir3/file').then(function(stats) {
+                stats.should.be.an('object')
+                stats.isFile().should.eql(true)
+                stats.size.should.be.eql(6)
+            })
+        })
+        .then(function() {
+            return riakfs1.readdir('/dir1').then(function(list) {
+                list.should.contain('file1').and.not.contain('dir2')
             })
         })
     })
