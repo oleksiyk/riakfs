ce264233bf4050a1fb8c5d4c0426bc211912d6d0
Oleksiy Krivoshey <oleksiy@luckyteam.co.uk>
2014-03-31 22:16:36 +0300

    Fix delete event not sent when moved to Trash

--- test/trash.js
+++ test/trash.js
@@ -11,7 +11,7 @@ describe('Trash - #unlink', function() {
     var riakfs;
 
     before(function() {
-        return connect({trash: true}).then(function(_riakfs) {
+        return connect({trash: true, events: true}).then(function(_riakfs) {
             riakfs = _riakfs
         })
     })
@@ -64,6 +64,27 @@ describe('Trash - #unlink', function() {
         .then(function (list) {
             list.should.contain('file1')
             list.should.not.contain('file1.1')
+        })
+    })
+
+    it('should emit delete event when file is moved to Trash', function (done) {
+        riakfs.writeFile('/test1', 'hello').then(function () {
+            riakfs.once('delete', function(filename, info) {
+                try {
+                    filename.should.be.eql('/test1')
+                    info.should.be.an('object')
+                    info.should.have.property('mtime')
+                    info.should.have.property('ctime')
+                    info.should.have.property('size', 5)
+                    info.should.have.property('contentType')
+                    info.should.have.property('version', 0)
+                    info.should.have.property('id')
+                    done()
+                } catch (err){
+                    done(err)
+                }
+            })
+            riakfs.unlink('/test1')
         })
     })
 
